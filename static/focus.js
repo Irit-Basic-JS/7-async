28967eeebff38ecd2ac90ccf57546545a9731578
Straordinariu <sasha4983@mail.ru>
2021-11-26 19:06:30 +0500

    all

--- static/focus.js
+++ static/focus.js
@@ -5,37 +5,29 @@ const API = {
     buhForms: "/api3/buh",
 };
 
-function run() {
-    sendRequest(API.organizationList, (orgOgrns) => {
-        const ogrns = orgOgrns.join(",");
-        sendRequest(`${API.orgReqs}?ogrn=${ogrns}`, (requisites) => {
-            const orgsMap = reqsToMap(requisites);
-            sendRequest(`${API.analytics}?ogrn=${ogrns}`, (analytics) => {
-                addInOrgsMap(orgsMap, analytics, "analytics");
-                sendRequest(`${API.buhForms}?ogrn=${ogrns}`, (buh) => {
-                    addInOrgsMap(orgsMap, buh, "buhForms");
-                    render(orgsMap, orgOgrns);
-                });
-            });
+async function run() {
+    const orgOgrns = await sendRequest(API.organizationList);
+    const ogrns = orgOgrns.join(",");
+    Promise.all([API.orgReqs, API.analytics, API.buhForms]
+        .map(url => sendRequest(`${url}?ogrn=${ogrns}`)))
+        .then(([reqs, analytics, buh]) => {
+            const orgsMap = reqsToMap(reqs);
+            addInOrgsMap(orgsMap, analytics, "analytics");
+            addInOrgsMap(orgsMap, buh, "buhForms");
+            render(orgsMap, orgOgrns);
         });
-    });
 }
 
 run();
 
-function sendRequest(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                callback(JSON.parse(xhr.response));
-            }
+function sendRequest(url) {
+    return fetch(url).then(response => {
+        if (response.ok) {
+            return response.json()
+        } else {
+            alert(`${response.status} ${response.statusText}`)
         }
-    };
-
-    xhr.send();
+    })
 }
 
 function reqsToMap(requisites) {
@@ -44,37 +36,30 @@ function reqsToMap(requisites) {
         return acc;
     }, {});
 }
-
 function addInOrgsMap(orgsMap, additionalInfo, key) {
     for (const item of additionalInfo) {
         orgsMap[item.ogrn][key] = item[key];
     }
 }
-
 function render(organizationsInfo, organizationsOrder) {
     const table = document.getElementById("organizations");
     table.classList.remove("hide");
-
     const template = document.getElementById("orgTemplate");
     const container = table.querySelector("tbody");
-
     organizationsOrder.forEach((item) => {
         renderOrganization(organizationsInfo[item], template, container);
     });
 }
-
 function renderOrganization(orgInfo, template, container) {
     const clone = document.importNode(template.content, true);
     const name = clone.querySelector(".name");
     const indebtedness = clone.querySelector(".indebtedness");
     const money = clone.querySelector(".money");
     const address = clone.querySelector(".address");
-
     name.textContent =
         (orgInfo.UL && orgInfo.UL.legalName && orgInfo.UL.legalName.short) ||
         "";
     indebtedness.textContent = formatMoney(orgInfo.analytics.s1002 || 0);
-
     if (
         orgInfo.buhForms &&
         orgInfo.buhForms.length &&
@@ -86,31 +71,25 @@ function renderOrganization(orgInfo, template, container) {
                 orgInfo.buhForms[orgInfo.buhForms.length - 1].form2[0] &&
                 orgInfo.buhForms[orgInfo.buhForms.length - 1].form2[0]
                     .endValue) ||
-                0
+            0
         );
     } else {
         money.textContent = "—";
     }
-
     const addressFromServer = orgInfo.UL.legalAddress.parsedAddressRF;
     address.textContent = createAddress(addressFromServer);
-
     container.appendChild(clone);
 }
-
 function formatMoney(money) {
     let formatted = money.toFixed(2);
     formatted = formatted.replace(".", ",");
-
     const rounded = money.toFixed(0);
     const numLen = rounded.length;
     for (let i = numLen - 3; i > 0; i -= 3) {
         formatted = `${formatted.slice(0, i)} ${formatted.slice(i)}`;
     }
-
     return `${formatted} ₽`;
 }
-
 function createAddress(address) {
     const addressToRender = [];
     if (address.regionName) {
@@ -125,9 +104,7 @@ function createAddress(address) {
     if (address.house) {
         addressToRender.push(createAddressItem("house"));
     }
-
     return addressToRender.join(", ");
-
     function createAddressItem(key) {
         return `${address[key].topoShortName}. ${address[key].topoValue}`;
     }
